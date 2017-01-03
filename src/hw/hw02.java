6e9b0e616671dd858ddd979e17b797b68ea98332
j8761336 <105021026@live.asia.edu.tw>
2017-01-03 22:27:04 +0800

    0.0

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 數列一:1 2 4 7 11 16 22 29…
 數列二:1 2 2 3 3 3 4 4 4 4 5 5 5 5 5...
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021026 詹佩郡
  */
 public class hw02 {
 
